09436a43163f8b5a24c1d8456d1a7f82b5ef37a7
Anton Strizhov <anton.strizhov@gmail.com>
2013-07-05 03:56:20 -0700

    finished explosions

--- Boost/src/org/pentode/boost/Boost.java
+++ Boost/src/org/pentode/boost/Boost.java
@@ -2,6 +2,11 @@ package org.pentode.boost;
 
 import java.util.Iterator;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
@@ -53,16 +58,33 @@ public class Boost implements ApplicationListener {
 
 	   Array<Body> walls;
 	   Body [] bombs;
+	   int [] givenCountdownTime;
+	   int [] countdownTime;
 	   float [][] coordB = {{5, 5}, {7, 5}};
+	   
+	   Array<Body []> explosions = new Array<Body []>();
+	   List<Integer> cleanupDelay = new ArrayList<Integer>();
 	   
 	   @Override
 	   public void create() {
+		   Gdx.app.log("MyTag", "my informative message");
+
 		   camera = new OrthographicCamera();
 		   camera.setToOrtho(false, 800, 480);
 		   Gdx.input.setInputProcessor(inputProcessor);
 		   
 		   walls = new Array<Body>();
 		   bombs = new Body[coordB.length];
+		   countdownTime = new int[coordB.length];
+		   givenCountdownTime = new int[coordB.length];
+		   
+		   givenCountdownTime[0] = 100;
+		   givenCountdownTime[1] = 300;
+		   
+
+		   for (int j = 0; j < coordB.length; j++) {
+			   countdownTime[j] = givenCountdownTime[j];
+		   }
 		   
 		   float [][] coord = {{16, 7, 0.1f, 7}, {0.1f, 7, 0.1f, 7}, {0, 0.1f, camera.viewportWidth, 0.1f}};
 		  
@@ -87,29 +109,10 @@ public class Boost implements ApplicationListener {
 			   walls.add(wallBody);
 		   }
 		   
-		   BodyDef bombDef;	   
 		   Body bomb;
-		   FixtureDef bombFixtureDef;
-		   Fixture bombFixture;
 		   
 		   for (int i = 0; i < coordB.length; i++) {
-			   bombDef = new BodyDef();
-			   bombDef.type = BodyType.DynamicBody;
-			   bombDef.position.set(new Vector2(coordB[i][0], coordB[i][1]));
-			   
-			   PolygonShape bombBox;
-			   bombBox = new PolygonShape();
-			   bombBox.setAsBox(0.5f, 0.5f);
-			   
-			   bombFixtureDef = new FixtureDef();
-			   bombFixtureDef.shape = bombBox;
-			   bombFixtureDef.density = 0.5f; 
-			   bombFixtureDef.friction = 0.4f;
-			   bombFixtureDef.restitution = 0.3f;
-			   
-			   bomb = world.createBody(bombDef);
-			   bombFixture = bomb.createFixture(bombFixtureDef);
-			   bombBox.dispose(); 
+			   bomb = createBomb(coordB[i][0], coordB[i][1]);
 			   
 			   bombs[i] = bomb;
 		   }
@@ -117,6 +120,33 @@ public class Boost implements ApplicationListener {
 		
 		   debugMatrix=new Matrix4(camera.combined);
 		   debugMatrix.scale(BOX_TO_WORLD, BOX_TO_WORLD, 1f);
+	   }
+	   
+	   private Body createBomb(float x, float y) {
+		   BodyDef bombDef;	   
+		   Body bomb;
+		   FixtureDef bombFixtureDef;
+		   Fixture bombFixture;
+		   
+		   bombDef = new BodyDef();
+		   bombDef.type = BodyType.DynamicBody;
+		   bombDef.position.set(new Vector2(x, y));
+		   
+		   PolygonShape bombBox;
+		   bombBox = new PolygonShape();
+		   bombBox.setAsBox(0.5f, 0.5f);
+		   
+		   bombFixtureDef = new FixtureDef();
+		   bombFixtureDef.shape = bombBox;
+		   bombFixtureDef.density = 0.5f; 
+		   bombFixtureDef.friction = 0.4f;
+		   bombFixtureDef.restitution = 0.3f;
+		   
+		   bomb = world.createBody(bombDef);
+		   bombFixture = bomb.createFixture(bombFixtureDef);
+		   bombBox.dispose(); 
+		   
+		   return bomb;
 	   }
 	   
 	   private void createBall() {
@@ -144,6 +174,7 @@ public class Boost implements ApplicationListener {
 	   private void explode(float blastPower, Vector2 center) {
 		   int numRays = 20;
 		   float DEGTORAD = (float)Math.PI/180;
+		   Body [] bullets = new Body[numRays];
 		   for (int i = 0; i < numRays; i++) {
 			      float angle = (i / (float)numRays) * 360 * DEGTORAD;
 			      Vector2 rayDir = new Vector2((float)Math.sin(angle),(float)Math.cos(angle));
@@ -167,9 +198,13 @@ public class Boost implements ApplicationListener {
 			      fd.friction = 0; // friction not necessary
 			      fd.restitution = 0.99f; // high restitution to reflect off obstacles
 			      fd.filter.groupIndex = -1; // particles should not collide with each other
-			      Fixture fixture;
-			      fixture = body.createFixture(fd);
+			      Fixture fixture = body.createFixture(fd);
+			      
+			      bullets[i] = body;
 			  }
+		   
+		   explosions.add(bullets);
+		   cleanupDelay.add(25);
 	   }
 	   
 	   @Override
@@ -180,16 +215,48 @@ public class Boost implements ApplicationListener {
 	      // process user input
 	      if(Gdx.input.isTouched()) {
 	    	  
-	    	  explode(20, new Vector2(6, 5));
 	    	  ball.applyForceToCenter(new Vector2(1, 1));
 	      }
 	      
+	      // cleaning up explosions
+	      
+	      int j;
+	      
+	      for (int i = 0; i < cleanupDelay.size(); i++) {
+	    	  j = cleanupDelay.get(i);
+	    	  cleanupDelay.set(i, j - 1);
+	    	  if (j - 1 == 0) {
+	    		  cleanupDelay.remove(i);
+	    		  cleanup(explosions.get(i));
+	    		  explosions.removeIndex(i);
+	    		  i -= 1;
+	    	  }
+	      }
+	      
+	      
+	      
 	      debugRenderer.render(world, debugMatrix);
 	      if (play) {
+	    	// Bomb timers count down
+		      
+		      for (int i = 0; i < coordB.length; i++) {
+		    	  countdownTime[i] -= 1;
+		    	  if (countdownTime[i] < 0) countdownTime[i] = -1;
+		    	  if (countdownTime[i] == 0) {
+		    		  explode(20, bombs[i].getPosition());
+		    		  world.destroyBody(bombs[i]);
+		    	  }
+		      }
+	    	  
 	    	  world.step(1/60f, 6, 2);
 	      }
 	   }
 	   
+	   private void cleanup(Body [] bull) {
+		   for (int k = 0; k < bull.length; k++) {
+			   world.destroyBody(bull[k]);
+		   }
+	   }
 	   
 	   public class MyInputProcessor implements InputProcessor {
 		   @Override
@@ -215,14 +282,15 @@ public class Boost implements ApplicationListener {
 		   @Override
 		   public boolean touchUp (int x, int y, int pointer, int button) {
 			   if(play) {
-		    		  resetLevel();
-		    	  }
+		    	  resetLevel();
+		      }
+			   
 		    	  play = !play;
 		      return false;
 		   }
 
 		   @Override
-		   public boolean touchDragged (int x, int y, int pointer) {
+		   public boolean touchDragged (int x, int y, int pointer) {			  
 		      return false;
 		   }
 
@@ -242,13 +310,27 @@ public class Boost implements ApplicationListener {
 		   ball.setLinearVelocity(new Vector2(0,0));
 		   ball.setAngularVelocity(0);
 		   ball.setAwake(true);
+		   
 		   Body bomb;
+		   
+		   for (int k = 0; k < coordB.length; k++) {
+			   if (countdownTime[k] <= 0) {
+				   bomb = createBomb(coordB[k][0], coordB[k][1]);
+				   bombs[k] = bomb;
+				   
+			   }
+		   }
+		   
 		   for (int i = 0; i < coordB.length; i++) {
 			   bomb = bombs[i];
 			   bomb.setTransform(coordB[i][0], coordB[i][1], 0);
 			   bomb.setLinearVelocity(new Vector2(0,0));
 			   bomb.setAngularVelocity(0);
 			   bomb.setAwake(true);
+		   }
+		   
+		   for (int j = 0; j < coordB.length; j++) {
+			   countdownTime[j] = givenCountdownTime[j];
 		   }
 	   }
 	   
